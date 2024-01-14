10e3f4cfe13af2914cb6065013662e026376ef52
mt02v <141305346+mt02v@users.noreply.github.com>
2024-01-14 15:57:35 +0000

    [Finish] Ajax_practice

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
+const API_KEY = "7e2726eec6d950450c0ef2f55cc70445";
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "7e2726eec6d950450c0ef2f55cc70445",
+      dataType : 'jsonp',
+    }).done(function (data){
+      //通信成功
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+      //通信失敗
+      alert('通信に失敗しました。');
+    });
   });
 });
